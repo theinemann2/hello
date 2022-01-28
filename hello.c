9f8d33cae52633a6bb5e90eb143defeb460e56e5
Toby Heinemann <theinemann2@huskers.unl.edu>
2022-01-28 16:08:21 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Toby Heinemann & Mike Maline
+ *       theinemann2@huskers.unl.edu
+ *      mmaline@huskers.unl.edu
+ * Date: 2020/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Toby Heinemann and Mike Maline\n");
+
+  return 0;
+}
